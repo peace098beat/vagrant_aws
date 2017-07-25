4b712d8b05915abb1771f5fa009a0bee17fae347
fifi <fifi@mail.com>
2017-07-26 07:56:11 +0900

    commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,9 @@
 # sudo pip3 install --upgrade keras
 # python3 ~/data/mnist_test.py
 
-shutdown -h +60
+# shutdown -h +60 | now+60min
+
+sudo pip3 install keras
 
 # # AWS cli
 source /vagrant/aws_configure.sh
@@ -12,17 +14,17 @@ aws configure list
 aws s3 ls
 
 # echo "Download Source Code"
-git clone https://github.com/peace098beat/keras_audio_cnn.git
+git clone https://github.com/peace098beat/keras_audio_cnn.git ~/keras_audio_cnn
 
 # echo "Download Datasets"
 # # Download Datasets
 # # wget https://s3-us-west-2.amazonaws.com/nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairend+magna/mfcc_npy/NoSairen_mfcc_2000.npy ./
 # # wget https://s3-us-west-2.amazonaws.com/nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairend+magna/mfcc_npy/OnSairen_mfcc_2000.npy ./
-aws s3 cp s3://nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairen+magna/mell_npy/NoSairen_mell_500.npy ./
-aws s3 cp s3://nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairen+magna/mell_npy/OnSairen_mell_500.npy ./
+aws s3 cp s3://nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairen+magna/mell_npy/NoSairen_mell_500.npy ~/
+aws s3 cp s3://nohara-dnn-workspace/audio-cnn-datasets/AudioCNN20170724/sairen+magna/mell_npy/OnSairen_mell_500.npy ~/
 
-mkdir ~/keras_audio_cnn/sairen+magna/
-cp ./*.npy ~/keras_audio_cnn/sairen+magna/
+mkdir ~/keras_audio_cnn/sairen+magna/mell_npy/
+cp ~/*.npy ~/keras_audio_cnn/sairen+magna/mell_npy/
 
 # echo "Analys Start .."
 python3 ~/keras_audio_cnn/training-cnn2d.py
